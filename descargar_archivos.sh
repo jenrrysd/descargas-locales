f2be1b988bfd9443a53a9f092d88d755ef5fb6f8
Dextre <dextre1481@gmail.com>
2024-03-30 19:33:16 +0000

    nuevo cambio para obtener ip

--- descargar_archivos.sh
+++ descargar_archivos.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #
-ip=$(hostname -I | cut -f1 -d" ")
+ip=$(ip route | awk '{print $9}' | head -1)
 echo "------------------------------------------------------------------------------"
 read -p "Porfavor ingresa la ruta de la carpeta a compartir; " ruta
-echo "En el navegador porner la siguiente dirección; $ip:9090 "
+echo "En el navegador porner la siguiente dirección; $ip:8090 "
 echo "------------------------------------------------------------------------------"
 echo "------ Compartiendo desde la ruta; $ruta ----------------"
 echo "------------------------------------------------------------------------------"
-cd $ruta && python -m http.server 9090
-
+cd $ruta && python -m http.server 8090
